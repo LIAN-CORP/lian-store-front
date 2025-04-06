5ebcbc23aa13f790e3d76091e628d9cb3433d1dd
Fsxerath <frediangarita1999@gmail.com>
2025-04-06 12:17:53 -0500

    init commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Button class="algo" label="verify" />
+    <div class="card flex justify-center">
+      <InputOtp v-model="value" :length="5" />
+    </div>
+  </div>
+</template>
+
+<style>
+.algo {
+  background-color: blue;
+}
+</style>
+<script setup>
+import { ref } from "vue";
+
+const value = ref(null);
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { customPreset } from "./theme/theme.preset";
+
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["@primevue/nuxt-module"],
+  css: ["/assets/styles/_theme.scss"],
+  primevue: {
+    options: {
+      theme: {
+        preset: customPreset,
+        darkModeSelector: false,
+        cssLayer: {
+          name: "primevue",
+          order: "app-styles, primevue",
+        },
+      },
+    },
+  },
+});
