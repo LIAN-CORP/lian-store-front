f7afe5d0a8f37daec9486c7ffa21e9f04c08e017
Fsxerath <frediangarita1999@gmail.com>
2025-04-06 14:14:38 -0500

    fix (typography.scss): delete font styles and change the font size

--- app.vue
+++ app.vue
@@ -2,6 +2,12 @@
   <div>
     <Button label="verify" />
     <div class="card flex justify-center">
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam illo
+        dolor dicta consequuntur, deleniti sapiente sit est, eius asperiores
+        dolorem porro aspernatur! Cumque assumenda, vitae iusto cum tempore
+        fugit dolorum!
+      </p>
       <InputOtp v-model="value" :length="5" />
     </div>
   </div>
