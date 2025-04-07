cf3d5c50284d86f6cc4fb2af502fe8d37dd2c884
Fsxerath <frediangarita1999@gmail.com>
2025-04-07 16:28:33 -0500

    feat (menu-bar): edit styles of the primevue component Menu bar

--- app.vue
+++ app.vue
@@ -1,23 +1,8 @@
 <template>
-  <div>
-    <Button label="verify" />
-    <div class="card flex justify-center">
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam illo
-        dolor dicta consequuntur, deleniti sapiente sit est, eius asperiores
-        dolorem porro aspernatur! Cumque assumenda, vitae iusto cum tempore
-        fugit dolorum!
-      </p>
-      <InputOtp v-model="value" :length="5" />
-    </div>
-  </div>
+  <nav-bar />
 </template>
 
-<style>
-.algo {
-  background-color: blue;
-}
-</style>
+<style lang="scss" scoped></style>
 <script setup>
 import { ref } from "vue";
 
---
+++ components/nav-bar.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const items = ref([
+  {
+    label: "Transacción",
+    icon: "pi pi-home",
+  },
+  {
+    label: "Inventario",
+    icon: "pi pi-star",
+  },
+  {
+    label: "Deudores",
+    icon: "pi pi-search",
+  },
+  {
+    label: "Informes",
+    icon: "pi pi-envelope",
+  },
+]);
+</script>
+
+<template>
+  <div>
+    <Menubar :model="items">
+      <template #start>
+        <h1>Lian Corp</h1>
+      </template>
+    </Menubar>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  color: #ffffff;
+  font-family: "LibreBarCode128";
+  font-size: 3rem;
+}
+</style>
