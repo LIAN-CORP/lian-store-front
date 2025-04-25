f94ef688f201c749969bbedf92b60b73d2a85d98
Fsxerath <frediangarita1999@gmail.com>
2025-04-25 08:51:54 -0500

    feat(movements): add select options and data table to movements page

--- pages/movements/index.vue
+++ pages/movements/index.vue
@@ -1,5 +1,27 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const selectedCity = ref();
+const options = ref([
+  { name: "Ventas" },
+  { name: "Compras" },
+  { name: "Deudas a cobrar" },
+]);
+</script>
 
-<template></template>
+<template>
+  <section class="movements">
+    <Select
+      :options="options"
+      option-label="name"
+      placeholder="Seleccione el tipo de movimiento"
+    ></Select>
+  </section>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.p-select {
+  width: 100%;
+}
+.movements {
+  padding: 1rem 4rem;
+}
+</style>
