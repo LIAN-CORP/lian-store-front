cebc0de4cfa7afd4830f7398a465131e130c5a40
Fsxerath <frediangarita1999@gmail.com>
2025-04-25 09:57:36 -0500

    feat(movements): fix variable name and adjust layout for movements page

--- pages/movements/index.vue
+++ pages/movements/index.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-const selectedCity = ref();
+const selectedItem = ref();
 const options = ref([
   { name: "Ventas" },
   { name: "Compras" },
@@ -13,7 +13,9 @@ const options = ref([
       :options="options"
       option-label="name"
       placeholder="Seleccione el tipo de movimiento"
+      v-model="selectedItem"
     ></Select>
+    <div class="movements-content"></div>
   </section>
 </template>
 
@@ -22,6 +24,24 @@ const options = ref([
   width: 100%;
 }
 .movements {
-  padding: 1rem 4rem;
+  padding: 1rem 3rem;
+  &-content {
+    display: flex;
+    flex-direction: column;
+    margin-top: 1rem;
+    background-color: rgb(255, 255, 255);
+    border-radius: 1rem;
+    width: 100%;
+    height: 75vh;
+    overflow-y: auto;
+    box-shadow: 0px -4px 15px -3px rgba(0, 0, 0, 0.05),
+      0px 5px 15px -3px rgba(0, 0, 0, 0.1);
+  }
+}
+
+@media (width < 500px) {
+  .movements {
+    padding: 1rem;
+  }
 }
 </style>
