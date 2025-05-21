203074da97622187f44c938c123c1cc296c0e282
Fsxerath <frediangarita1999@gmail.com>
2025-05-21 16:06:52 -0500

    feat(movements): add date pickers and data table for movement records

--- pages/movements/index.vue
+++ pages/movements/index.vue
@@ -1,21 +1,55 @@
 <script lang="ts" setup>
+import { Icon } from "@iconify/vue/dist/iconify.js";
+
 const selectedItem = ref();
 const options = ref([
   { name: "Ventas" },
   { name: "Compras" },
   { name: "Deudas a cobrar" },
 ]);
+const payments = ref();
 </script>
 
 <template>
   <section class="movements">
+    <article class="movements-header">
+      <IftaLabel>
+        <DatePicker fluid input-id="start_date" show-icon />
+        <label for="start_date">Fecha Inicial</label>
+      </IftaLabel>
+      <IftaLabel>
+        <DatePicker fluid input-id="end_date" show-icon />
+        <label for="end_date">Fecha Final</label>
+      </IftaLabel>
+      <Button label="Buscar" severity="warn" />
+    </article>
     <Select
       :options="options"
       option-label="name"
       placeholder="Seleccione el tipo de movimiento"
       v-model="selectedItem"
     ></Select>
-    <div class="movements-content"></div>
+    <div class="movements-content">
+      <DataTable :value="payments" paginator :rows="9">
+        <Column field="date" header="Fecha" />
+        <Column field="id" header="Factura" />
+        <Column field="total" header="Total" />
+        <Column field="actions" header="Acciones">
+          <template #body>
+            <Button variant="text" severity="info">
+              <template #icon>
+                <Icon
+                  icon="lets-icons:chat-search"
+                  width="24"
+                  height="24"
+                  style="color: #172455"
+                />
+              </template>
+            </Button>
+          </template>
+        </Column>
+      </DataTable>
+    </div>
   </section>
 </template>
 
@@ -24,24 +58,28 @@ const options = ref([
   width: 100%;
 }
 .movements {
-  padding: 1rem 3rem;
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+  gap: 1rem;
+  &-header {
+    display: flex;
+    justify-content: space-between;
+    gap: 1rem;
+  }
   &-content {
     display: flex;
     flex-direction: column;
-    margin-top: 1rem;
-    background-color: rgb(255, 255, 255);
-    border-radius: 1rem;
-    width: 100%;
-    height: 75vh;
-    overflow-y: auto;
-    box-shadow: 0px -4px 15px -3px rgba(0, 0, 0, 0.05),
-      0px 5px 15px -3px rgba(0, 0, 0, 0.1);
   }
 }
 
-@media (width < 500px) {
+@media (width < 800px) {
   .movements {
     padding: 1rem;
+    &-header {
+      flex-direction: column;
+      gap: 0.5rem;
+    }
   }
 }
 </style>
