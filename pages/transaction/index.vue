61645ae47093b92a3657db46f7d16c6044ca7f08
Fsxerath <frediangarita1999@gmail.com>
2025-06-03 07:15:50 -0500

    feat(transaction): add transaction page

---
+++ pages/transaction/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="transaction">
+    <TransactionSelectProduct />
+    <div>esto tiene algo</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.transaction {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  width: 100%;
+}
+</style>
