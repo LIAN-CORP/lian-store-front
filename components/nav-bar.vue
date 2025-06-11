c5ecade379c3a984a8e7faaed051b1a068341629
Fredy-Angarita <frediangarita1999@gmail.com>
2025-06-11 13:16:49 -0500

    fix(nav-bar): add menu item to logout and active style to menu item

--- components/nav-bar.vue
+++ components/nav-bar.vue
@@ -1,20 +1,30 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import { Icon } from "@iconify/vue";
 
 const router = useRouter();
+const route = useRoute();
 
+function isActive(itemRoute: string) {
+  return route.path == itemRoute;
+}
+
+onMounted(() => {
+  console.log("El componente se ha renderizado");
+});
 const items = ref([
   {
     label: "Transacción",
     icon: "hugeicons:transaction",
+    to: "/transaction",
+    class: "active_item",
     command: () => {
-      router.push("/transaction");
+      navigateTo("/transaction");
     },
   },
   {
     label: "Inventario",
     icon: "si:inventory-fill",
+    to: "/inventory",
     command: () => {
       router.push("/inventory");
     },
@@ -22,6 +32,7 @@ const items = ref([
   {
     label: "Deudores",
     icon: "lucide:book-user",
+    to: "/debts",
     command: () => {
       router.push("/debts");
     },
@@ -29,21 +40,46 @@ const items = ref([
   {
     label: "Movimientos",
     icon: "mdi:report-box",
+    to: "/movements",
     command: () => {
       router.push("/movements");
     },
+  },
+  {
+    label: "salir",
+    type: "logout",
   },
 ]);
 </script>
 
 <template>
   <div>
-    <Menubar :model="items">
+    <Menubar
+      :model="items"
+      :pt="{
+        itemContent: ({ context }) => ({
+          style: isActive(context.item.to) ? 'background-color: #4155a4' : '',
+        }),
+      }"
+      breakpoint="1000px"
+    >
       <template #start>
         <h1>Lian Corp</h1>
       </template>
       <template #itemicon="{ item }">
-        <Icon :icon="item.icon!" width="1.5em" height="1.5em" />
+        <Icon
+          v-if="!item.type"
+          :icon="item.icon!"
+          width="1.5em"
+          height="1.5em"
+        />
+        <Icon
+          v-if="item.type == 'logout'"
+          icon="streamline:logout-1-solid"
+          width="1.3em"
+          height="1.3em"
+          style="color: #ff0101"
+        />
       </template>
     </Menubar>
   </div>
