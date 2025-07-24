18a1431036d774d8dd9515acccb719fbe0bc6985
Fredy-Angarita <frediangarita1999@gmail.com>
2025-07-24 07:11:17 -0500

    feat (components): create new text area field

---
+++ components/custom-text-area-field.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+withDefaults(
+  defineProps<{
+    id: string;
+    name: string;
+    label: string;
+    size?: string;
+    placeholder?: string;
+    pattern?: string;
+    inputColor?: string;
+    fluid?: boolean;
+    disabled?: boolean;
+    maxlength?: number;
+    minlength?: number;
+  }>(),
+  {
+    fluid: true,
+    inputColor: "#eef2ff",
+  }
+);
+</script>
+
+<template>
+  <div class="formField">
+    <FormField v-slot="$field" :name="name">
+      <FloatLabel variant="on">
+        <Textarea
+          :style="{ backgroundColor: inputColor }"
+          :id="id"
+          :name="name"
+          :fluid="fluid"
+          :placeholder="placeholder"
+          :size="size"
+          :disabled="disabled"
+        />
+        <label :for="id">{{ label }}</label>
+      </FloatLabel>
+      <Message
+        v-if="$field.invalid"
+        variant="simple"
+        size="small"
+        severity="error"
+        >{{ $field.error?.message }}
+      </Message>
+    </FormField>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
