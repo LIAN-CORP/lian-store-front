7ffd9f1b82427853da88b35a88d7eeb6ded9c529
3lPr1mo <erickenriquechaparro@gmail.com>
2025-08-18 01:07:51 -0500

    feat(composables): update `useGetApiUrl` to support multiple API services

--- composables/use-get-api-url.ts
+++ composables/use-get-api-url.ts
@@ -1,4 +1,12 @@
-export default function useGetApiUrl(path: string): string {
-  const url = useRuntimeConfig().public.apiBase;
-  return `${url}/${path}`;
+type ApiService = 'stockApi' | 'transactionApi';
+
+export default function useGetApiUrl(path: string, service: ApiService): string {
+    const config = useRuntimeConfig().public;
+    const apis = {
+        stockApi: config.stockApi,
+        transactionApi: config.transactionApi,
+    }
+    return `${apis[service]}/${path}`;
+    //const url = useRuntimeConfig().public.stockApi;
+  //return `${url}/${path}`;
 }
