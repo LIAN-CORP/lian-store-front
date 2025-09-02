9c95ce874d17211d8d9be2ce80d3f78641f689a7
Fredy-Angarita <frediangarita1999@gmail.com>
2025-09-02 18:54:21 -0500

    feat (components): create wrapper of primevue maskInput

---
+++ components/custom-mask-input.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+const props = withDefaults(
+  defineProps<{
+    id: string;
+    name: string;
+    label: string;
+    mask: string;
+    size?: string;
+    placeholder?: string;
+    inputColor?: string;
+    fluid?: boolean;
+    disabled?: boolean;
+  }>(),
+  {
+    fluid: true,
+    inputColor: "#eef2ff",
+  }
+);
+
+const { value, errorMessage } = useField<string>(() => props.name);
+</script>
+
+<template>
+  <div class="formField">
+    <IftaLabel>
+      <InputMask
+        :mask="mask"
+        :style="{ backgroundColor: inputColor }"
+        :id="id"
+        :name="name"
+        :placeholder="placeholder"
+        :fluid="fluid"
+        :size="size"
+        :disabled="disabled"
+        v-model:model-value="value"
+      />
+      <label :for="id">{{ label }}</label>
+    </IftaLabel>
+    <Message
+      v-if="errorMessage"
+      variant="simple"
+      size="small"
+      severity="error"
+      >{{ errorMessage }}</Message
+    >
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
