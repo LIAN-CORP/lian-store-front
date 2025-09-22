9363f65c26b9ea15b673d6012531d6e54ae1ac34
Fredy-Angarita <frediangarita1999@gmail.com>
2025-09-22 16:32:39 -0500

    refactor(composables): add ApiFetch composable to send request

--- composables/inventory/product/use-new-product.ts
+++ composables/inventory/product/use-new-product.ts
@@ -1,39 +1,36 @@
-import type { ErrorResponse } from "~/interfaces/error.response";
 import type { NewProductRequest } from "~/interfaces/inventory/product/request/new.product.request";
 
 export default function useNewProduct() {
-  const token = useCookie("access_token");
   const loading = ref<boolean>(false);
   const { errorToast, successToast } = useCreateToast();
   const { getErrorTranslate, getSuccessTranslate } = useHandleResponse();
 
   async function createNewProduct(image: File, product: NewProductRequest) {
-    const url = useGetApiUrl("product");
-    let msg: string;
     const formData = new FormData();
     const jsonBlob = new Blob([JSON.stringify(product)], {
       type: "application/json",
     });
     formData.append("image", image);
     formData.append("product", jsonBlob);
-    try {
-      loading.value = true;
-      await fetch(url, {
-        method: "POST",
-        body: formData,
-        headers: {
-          Authorization: token.value ? `Bearer ${token.value}` : "",
-        },
-      });
-      msg = getSuccessTranslate("response.success.new_product");
-      successToast(msg);
-    } catch (e: any) {
-      const error = e.data as ErrorResponse;
-      msg = getErrorTranslate(error.type);
+    const {
+      error,
+      execute,
+      loading: load,
+    } = useApiFetch("product", {
+      method: "POST",
+      body: formData,
+    });
+    watch(load, (val) => {
+      loading.value = val;
+    });
+    await execute();
+    if (error.value) {
+      const msg = getErrorTranslate(error.value.type);
       errorToast(msg);
-    } finally {
-      loading.value = false;
+      return;
     }
+    const msg = getSuccessTranslate("response.success.new_product");
+    successToast(msg);
   }
   return {
     createNewProduct,
--- composables/inventory/product/use-update-product.ts
+++ composables/inventory/product/use-update-product.ts
@@ -1,34 +1,30 @@
-import type { ErrorResponse } from "~/interfaces/error.response";
 import type { UpdateProductRequest } from "~/interfaces/inventory/product/request/update.product.request";
 
 export default function useUpdateProduct() {
-  const token = useCookie("access_token");
-
   const loading = ref<boolean>(false);
   const { errorToast, infoToast } = useCreateToast();
   const { getErrorTranslate, getSuccessTranslate } = useHandleResponse();
 
   async function update(updatedProduct: UpdateProductRequest) {
-    const url = useGetApiUrl("product");
-    let msg: string;
-    try {
-      loading.value = true;
-      await $fetch.raw(url, {
-        method: "PUT",
-        body: updatedProduct,
-        headers: {
-          Authorization: token.value ? `Bearer ${token.value}` : "",
-        },
-      });
-      msg = getSuccessTranslate("response.success.update_product");
-      infoToast(msg);
-    } catch (e: any) {
-      const error = e as ErrorResponse;
-      msg = getErrorTranslate(error.type);
+    const {
+      loading: load,
+      error,
+      execute,
+    } = useApiFetch("product", {
+      method: "PUT",
+      body: updatedProduct,
+    });
+    watch(load, (val) => {
+      loading.value = val;
+    });
+    await execute();
+    if (error.value) {
+      const msg = getErrorTranslate(error.value.type);
       errorToast(msg);
-    } finally {
-      loading.value = false;
+      return;
     }
+    const msg = getSuccessTranslate("response.success.update_product");
+    infoToast(msg);
   }
   return {
     update,
